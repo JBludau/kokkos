88fbe41a07eb080c5889d9e981fd37a14925d109
JBludau <104908666+JBludau@users.noreply.github.com>
2025-01-15 18:08:40 -0500

    Update example/build_installed/example_source/lib_without_kokkos_dependency/foo.cpp
    Co-authored-by: Damien L-G <dalg24+github@gmail.com>

--- example/build_installed/example_source/lib_without_kokkos_dependency/foo.cpp
+++ example/build_installed/example_source/lib_without_kokkos_dependency/foo.cpp
@@ -17,5 +17,5 @@
 #include <iostream>
 
 void print_foo() {
-  std::cout << "Hello from foo within library without kokkos dependence \n";
+  std::cout << "Hello from foo within library without kokkos dependency \n";
 }
