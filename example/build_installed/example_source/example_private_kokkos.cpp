f109a33503054e8a5a09413378ae45dca3d7277c
JBludau <jakob.bludau@tum.de>
2025-01-14 19:19:37 -0500

    moved files and prefixed target names to prevent clashing

---
+++ example/build_installed/example_source/example_private_kokkos.cpp
@@ -0,0 +1,28 @@
+//@HEADER
+// ************************************************************************
+//
+//                        Kokkos v. 4.0
+//       Copyright (2022) National Technology & Engineering
+//               Solutions of Sandia, LLC (NTESS).
+//
+// Under the terms of Contract DE-NA0003525 with NTESS,
+// the U.S. Government retains certain rights in this software.
+//
+// Part of Kokkos, under the Apache License v2.0 with LLVM Exceptions.
+// See https://kokkos.org/LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//@HEADER
+
+#include <cstdio>
+#include <iostream>
+
+extern "C" void print_fortran_();
+void print_furr();
+void print_foo();
+
+int main(int argc, char* argv[]) {
+  print_furr();
+  print_foo();
+  print_fortran_();
+}
