4ad72d4bb1663a44bf9dffc6e2f497fb1cc1fc02
Jakob Bludau <jakob.bludau@tum.de>
2025-06-23 12:19:43 -0400

    build multilanguage example in jenkins
    Signed-off-by: Jakob Bludau <jakob.bludau@tum.de>
    Signed-off-by: Jakob Bludau <bludauj@ornl.gov>

--- .jenkins/continuous.groovy
+++ .jenkins/continuous.groovy
@@ -174,6 +174,7 @@ pipeline {
                                 -DKokkos_ENABLE_CUDA_UVM=ON \
                                 -DKokkos_ENABLE_CUDA_RELOCATABLE_DEVICE_CODE=ON \
                                 -DKokkos_ENABLE_DEPRECATED_CODE_4=ON \
+                                -DKokkos_ENABLE_MULTIPLE_CMAKE_LANGUAGES=ON \
                                 \
                                 -DCMAKE_INSTALL_PREFIX=${PWD}/../install \
                               .. && \
@@ -197,6 +198,14 @@ pipeline {
                                 -DCMAKE_CXX_COMPILER=g++-11 \
                                 -DCMAKE_CXX_FLAGS=-Werror \
                                 -DCMAKE_CXX_STANDARD=20 \
+                              .. && \
+                              make -j8 && ctest --verbose && \
+                              cd ../build_cmake_installed_multilanguage && \
+                              rm -rf build && mkdir -p build && cd build && \
+                              cmake \
+                                -DCMAKE_CXX_COMPILER=g++-8 \
+                                -DCMAKE_CXX_FLAGS=-Werror \
+                                -DCMAKE_CXX_STANDARD=17 \
                               .. && \
                               make -j8 && ctest --verbose && \
                               cd ../.. && \
