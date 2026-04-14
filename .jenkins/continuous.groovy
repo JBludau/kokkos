26def9189045cd83a325bc77d1f3af29cde7a7e9
Jakob Bludau <bludauj@ornl.gov>
2026-04-14 16:37:45 -0400

    switch to old installer for more information
    Signed-off-by: Jakob Bludau <bludauj@ornl.gov>

--- .jenkins/continuous.groovy
+++ .jenkins/continuous.groovy
@@ -309,6 +309,7 @@ pipeline {
                           rm -rf spack && \
                           git clone https://github.com/spack/spack.git && \
                           . ./spack/share/spack/setup-env.sh && \
+                          spack config add config:installer:old && \
                           spack install -v --only=dependencies kokkos@develop+rocm+tests amdgpu_target=gfx90a ^hip@7 && \
                           spack install -v --only=package ${CDASH_ARGS} kokkos@develop+rocm+tests amdgpu_target=gfx90a ^hip@7 && \
                           spack load cmake  && \
