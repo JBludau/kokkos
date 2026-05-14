b1e91aca83ae3d82536f33db52308ebf5f89afb0
Jakob Bludau <bludauj@ornl.gov>
2026-05-14 08:40:37 -0400

    load hip package
    Signed-off-by: Jakob Bludau <bludauj@ornl.gov>

--- .jenkins/continuous.groovy
+++ .jenkins/continuous.groovy
@@ -66,7 +66,7 @@ pipeline {
                           spack install -v --only=dependencies kokkos@develop+rocm+tests amdgpu_target=gfx90a ^hip@7 && \
                           spack install -v --only=package ${CDASH_ARGS} kokkos@develop+rocm+tests amdgpu_target=gfx90a ^hip@7 && \
                           spack load cmake  && \
-                          spack load rocm && \
+                          spack load hip && \
                           spack load kokkos && \
                           spack test run ${CDASH_ARGS} kokkos && \
                           spack test results -l
