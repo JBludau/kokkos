5082c088dab7d842c0958b8cab80f7c7cdec44ab
Jakob Bludau <bludauj@ornl.gov>
2026-07-06 10:46:47 -0400

    make memcmp an inline function
    Signed-off-by: Jakob Bludau <bludauj@ornl.gov>

--- core/src/impl/Kokkos_Memcmp.hpp
+++ core/src/impl/Kokkos_Memcmp.hpp
@@ -10,7 +10,19 @@
 
 namespace Kokkos {
 namespace Impl {
-KOKKOS_FUNCTION int memcmp(void const* lhs, void const* rhs, std::size_t count);
+KOKKOS_INLINE_FUNCTION int memcmp(void const* lhs, void const* rhs,
+                                  std::size_t count) {
+  auto u1 = static_cast<unsigned char const*>(lhs);
+  auto u2 = static_cast<unsigned char const*>(rhs);
+  while (count-- != 0) {
+    if (*u1 != *u2) {
+      return (*u1 < *u2) ? -1 : +1;
+    }
+    ++u1;
+    ++u2;
+  }
+  return 0;
 }
+}  // namespace Impl
 }  // namespace Kokkos
 #endif
